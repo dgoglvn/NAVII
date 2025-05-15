92cfb59cb1f2a621da5552ecf1ccb9c60a60043f
Diego <91395111+dgoglvn@users.noreply.github.com>
2025-05-15 01:31:21 -0600

    small progress

--- links_awakening_env.py
+++ links_awakening_env.py
@@ -1,6 +1,6 @@
 """Link's Awakening gymnasium environment."""
 
-from typing import Dict
+from typing import Any, Literal, Optional
 
 import gymnasium as gym
 from gymnasium.spaces import Discrete
@@ -11,17 +11,19 @@ from pyboy.utils import WindowEvent
 class LinksAwakeningEnv(gym.Env):
     """Link's Awakening DX environment for reinforcement learning."""
 
-    def __init__(self, rom_path="LinksAwakeningDX.gbc"):
+    def __init__(self, rom_path: str = "LinksAwakeningDX.gbc") -> None:
         super().__init__()
 
-        self.pyboy = PyBoy(rom_path, window="SDL2")
-        self.pyboy.set_emulation_speed(2)
+        self.pyboy = PyBoy(
+            rom_path, window="SDL2"
+        )  # loads the game rom, runs it in a visible window (SDL2)
+        self.pyboy.set_emulation_speed(2)  # speeds up emulation (2x)
 
         # We have 8 actions, corresponding to "down", "left",
         # "right", "up", "a", "b", "select", "start"
         self.action_space = Discrete(8)
 
-        self.ACTIONS: Dict[int, int] = {
+        self.ACTIONS: dict[int, int] = {
             0: WindowEvent.PRESS_ARROW_DOWN,
             1: WindowEvent.PRESS_ARROW_LEFT,
             2: WindowEvent.PRESS_ARROW_RIGHT,
@@ -32,7 +34,13 @@ class LinksAwakeningEnv(gym.Env):
             7: WindowEvent.PRESS_BUTTON_START,
         }
 
-    def step(self, action_idx):
+    def reset(
+        self, seed: Optional[int] = None, options: Optional[dict] = None
+    ) -> tuple[Any, Any]:
+        # TODO
+        pass
+
+    def step(self, action_idx: Any) -> tuple[Any, Any, Any, Literal[False], dict]:
         """
         Take a step in the environment.
 
@@ -50,3 +58,19 @@ class LinksAwakeningEnv(gym.Env):
         self.pyboy.send_input(release_event)
         for _ in range(2):
             self.pyboy.tick()
+
+        observation = self._get_obs()
+        reward = self._get_reward()
+        terminated = self._check_done()
+
+        return observation, reward, terminated, False, {}
+
+    def _get_obs(self):
+        return
+
+    def _get_reward(self) -> int:
+        # placeholder reward logic: always return 0
+        return 0
+
+    def _check_done(self):
+        return
